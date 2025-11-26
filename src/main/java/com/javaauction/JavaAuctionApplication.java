316f67f28161d29f2e6663bc6fc0597d2b3e58ab
hangw2130\hangw <hangw2130@gmail.com>
2025-11-26 11:57:01 +0900

    init commit

---
+++ src/main/java/com/javaauction/JavaAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.javaauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAuctionApplication.class, args);
+	}
+
+}
