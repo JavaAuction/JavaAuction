964b5f73f68ae93eaf38117d7294e43bb9519505
fleurnari <fleur_nr@naver.com>
2025-11-28 11:32:04 +0900

    feat : alert DTO 생성

---
+++ alert-service/src/main/java/com/javaauction/alertservice/presentation/dto/request/ReqPostInternalAlertsDtoV1.java
@@ -0,0 +1,19 @@
+package com.javaauction.alertservice.presentation.dto.request;
+
+import com.javaauction.alertservice.domain.enums.AlertType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReqPostInternalAlertsDtoV1 {
+    private String userId;
+    private UUID productId;
+    private AlertType alertType;
+}
---
+++ alert-service/src/main/java/com/javaauction/alertservice/presentation/dto/response/RepGetAlertsDtoV1.java
@@ -0,0 +1,25 @@
+package com.javaauction.alertservice.presentation.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.javaauction.alertservice.domain.enums.AlertType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RepGetAlertsDtoV1 {
+    private UUID alertId;
+    private String productName;
+    private AlertType alertType;
+    private String content;
+    private boolean isRead;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+}
