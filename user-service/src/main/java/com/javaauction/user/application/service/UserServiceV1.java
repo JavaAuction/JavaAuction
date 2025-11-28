c538e013b922ac722167cd29a62b2ca8763885de
hangw2130\hangw <hangw2130@gmail.com>
2025-11-28 11:26:23 +0900

    사용자 목록조회, 단건조회

--- user-service/src/main/java/com/javaauction/user/application/service/UserServiceV1.java
+++ user-service/src/main/java/com/javaauction/user/application/service/UserServiceV1.java
@@ -6,12 +6,18 @@ import com.javaauction.global.presentation.exception.BussinessException;
 import com.javaauction.global.presentation.response.ApiResponse;
 import com.javaauction.user.application.dto.ReqLoginDto;
 import com.javaauction.user.application.dto.ReqSignupDto;
-import com.javaauction.user.application.dto.ResLoginDto;
-import com.javaauction.user.domain.repository.UserRepository;
 import com.javaauction.user.domain.entity.UserEntity;
+import com.javaauction.user.domain.repository.UserRepository;
 import com.javaauction.user.infrastructure.JWT.JwtUtil;
+import com.javaauction.user.presentation.dto.ResGetUserAdminDto;
+import com.javaauction.user.presentation.dto.ResGetUserDto;
+import com.javaauction.user.presentation.dto.ResLoginDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -42,6 +48,7 @@ public class UserServiceV1 {
                 .username(signupRequestDto.getUsername())
                 .password(passwordEncoder.encode(signupRequestDto.getPassword()))
                 .email(signupRequestDto.getEmail())
+                .name(signupRequestDto.getName())
                 .slackId(signupRequestDto.getSlackId())
                 .role(signupRequestDto.getRole())
                 .build();
@@ -86,5 +93,44 @@ public class UserServiceV1 {
             log.warn("로그인 실패 - username: {}, error: {}", loginRequestDto.getUsername(), e.getMessage());
             throw new BussinessException(BaseErrorCode.INVALID_INPUT_VALUE);
         }
+    }
+
+    public ApiResponse getAllUsers(int page, int size, String sortBy, boolean isAsc, String role) {
+        if(!role.equals("ADMIN")){
+            throw new BussinessException(BaseErrorCode.ACCESS_DENIED);
+        }
+        log.debug("유저 목록 조회 - page: {}, size: {}, sortBy: {}, isAsc: {}", page, size, sortBy, isAsc);
+
+        if(size != 10 && size != 30 && size != 50) {
+            size = 10;
+        }
+        if(!sortBy.equals("modifiedAt") || !sortBy.isEmpty() && !sortBy.isBlank()) {
+            sortBy = "createdAt";
+        }
+
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page>0?page-1:page, size, sort);
+
+        Page<UserEntity> userList = userRepository.getUsers(pageable);
+        log.info("유저 목록 조회 완료 - 총 {}건, 현재 페이지: {}", userList.getTotalElements(), page);
+
+        return ApiResponse.success(BaseSuccessCode.OK, userList.map(ResGetUserAdminDto::of));
+    }
+
+    public ApiResponse getUser(String userId, String role) {
+        UserEntity user = userRepository.findByUsername(userId).orElseThrow(() -> new BussinessException(BaseErrorCode.INVALID_INPUT_VALUE));
+
+        if(role.equals("ADMIN")){
+            return ApiResponse.success(BaseSuccessCode.OK, ResGetUserAdminDto.of(user));
+        }else{
+            return ApiResponse.success(BaseSuccessCode.OK, ResGetUserDto.of(user));
+        }
+    }
+
+    public ApiResponse getMyInfo(String username) {
+        UserEntity my = userRepository.findByUsername(username).orElseThrow(() -> new BussinessException(BaseErrorCode.INVALID_INPUT_VALUE));
+
+        return ApiResponse.success(BaseSuccessCode.OK, ResGetUserDto.of(my));
     }
 }
--- user-service/src/main/java/com/javaauction/user/infrastructure/repository/UserJpaRepository.java
+++ user-service/src/main/java/com/javaauction/user/infrastructure/repository/UserJpaRepository.java
@@ -3,6 +3,8 @@ package com.javaauction.user.infrastructure.repository;
 import com.javaauction.user.domain.entity.UserEntity;
 import jdk.jfr.Registered;
 import org.apache.catalina.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -12,4 +14,5 @@ public interface UserJpaRepository extends JpaRepository<UserEntity, String> {
     Optional<UserEntity> findByUsername(String username);
     Optional<UserEntity> findByEmail(String email);
     Optional<UserEntity> findBySlackId(String slackId);
+    Page<UserEntity> findAllByDeletedAtIsNull(Pageable pageable);
 }
