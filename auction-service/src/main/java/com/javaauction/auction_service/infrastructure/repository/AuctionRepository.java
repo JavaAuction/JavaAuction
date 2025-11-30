7968a7c330a18f8cab86ea30b0a3c92324f69c2d
cicle00 <cicle1999@gmail.com>
2025-11-30 12:41:09 +0900

    Revert "Feat/#4 bids"

--- auction-service/src/main/java/com/javaauction/auction_service/infrastructure/repository/AuctionRepository.java
+++ auction-service/src/main/java/com/javaauction/auction_service/infrastructure/repository/AuctionRepository.java
@@ -1,23 +1,13 @@
 package com.javaauction.auction_service.infrastructure.repository;
 
 import com.javaauction.auction_service.domain.entity.Auction;
-
 import java.util.Optional;
 import java.util.UUID;
-
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface AuctionRepository extends JpaRepository<Auction, UUID>, AuctionQuerydslRepository {
 
     boolean existsByProductIdAndDeletedAtIsNull(UUID productId);
 
     Optional<Auction> findByAuctionIdAndDeletedAtIsNull(UUID auctionId);
-
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("select a from Auction a where a.auctionId = :auctionId")
-    Optional<Auction> findByIdForUpdate(@Param("auctionId") UUID auctionId);
 }
