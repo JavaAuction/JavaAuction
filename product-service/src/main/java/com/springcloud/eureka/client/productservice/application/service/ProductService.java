da3a039bae5b6b6710073f2893c6b35b2c7fbd8d
banasu0723 <banasu0723@gmail.com>
2025-12-01 15:28:57 +0900

    feat: Product 페이징(키워드, 상태, 페이징 필터링) 조회및 상품명 검색api 구현

--- product-service/src/main/java/com/springcloud/eureka/client/productservice/application/service/ProductService.java
+++ product-service/src/main/java/com/springcloud/eureka/client/productservice/application/service/ProductService.java
@@ -6,6 +6,7 @@ import com.springcloud.eureka.client.productservice.domain.enums.ProductStatus;
 import com.springcloud.eureka.client.productservice.domain.error.ProductErrorCode;
 import com.springcloud.eureka.client.productservice.infrastructure.repository.ProductRepository;
 import com.springcloud.eureka.client.productservice.presentation.dto.RepProductDto;
+import com.springcloud.eureka.client.productservice.presentation.dto.RepProductPageDto;
 import com.springcloud.eureka.client.productservice.presentation.dto.ReqProductCreateDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -41,5 +42,36 @@ public class ProductService {
         return RepProductDto.from(product);
     }
 
+    // 상품 목록 조회 (검색/상태, 카테고리는 나중에)
+    @Transactional(readOnly = true)
+    public RepProductPageDto getProducts(String keyword, ProductStatus status, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Product> result;
+
+        if (keyword == null || keyword.isBlank()) {
+            if (status == null) {
+                result = productRepository.findAll(pageable);
+            } else {
+                result = productRepository.findByStatus(status, pageable);
+            }
+        } else {
+            if (status == null) {
+                result = productRepository.findByNameContainingIgnoreCase(keyword, pageable);
+            } else {
+                result = productRepository.findByNameContainingIgnoreCaseAndStatus(keyword, status, pageable);
+            }
+        }
+
+        Page<RepProductDto> mapped = result.map(RepProductDto::from);
+        return RepProductPageDto.from(mapped);
+    }
+
+    // 상품명 조회
+    @Transactional(readOnly = true)
+    public String getProductName(UUID productId) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new BussinessException(ProductErrorCode.PRODUCT_NOT_FOUND));
+        return product.getName();
+    }
 
 }
