30553192223f0902e1a5591d4bbb9b00cf0cf753
banasu0723 <banasu0723@gmail.com>
2025-12-01 11:31:20 +0900

    feat: Product 단건 조회 구현

--- product-service/src/main/java/com/springcloud/eureka/client/productservice/application/service/ProductService.java
+++ product-service/src/main/java/com/springcloud/eureka/client/productservice/application/service/ProductService.java
@@ -1,13 +1,20 @@
 package com.springcloud.eureka.client.productservice.application.service;
 
+import com.javaauction.global.presentation.exception.BussinessException;
 import com.springcloud.eureka.client.productservice.domain.entity.Product;
+import com.springcloud.eureka.client.productservice.domain.enums.ProductStatus;
+import com.springcloud.eureka.client.productservice.domain.error.ProductErrorCode;
 import com.springcloud.eureka.client.productservice.infrastructure.repository.ProductRepository;
-import com.springcloud.eureka.client.productservice.presentation.dto.RepProductCreateDto;
+import com.springcloud.eureka.client.productservice.presentation.dto.RepProductDto;
 import com.springcloud.eureka.client.productservice.presentation.dto.ReqProductCreateDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.util.UUID;
 
 @Service
 @Transactional
@@ -20,11 +27,19 @@ public class ProductService {
     }
 
     // 상품 생성
-    public RepProductCreateDto createProduct(String userId, ReqProductCreateDto request){
+    public RepProductDto createProduct(String userId, ReqProductCreateDto request){
         Product product = request.toEntity(userId);
         product.setCreate(Instant.now(), userId);
         Product saved = productRepository.save(product);
-        return RepProductCreateDto.from(saved);
+        return RepProductDto.from(saved);
     }
+
+    // 상품 단건 조회
+    public RepProductDto getProduct(UUID productId){
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new BussinessException(ProductErrorCode.PRODUCT_NOT_FOUND));
+        return RepProductDto.from(product);
+    }
+
 
 }
