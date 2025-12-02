129d81b1a6a1d05cefd83ef6d17b4673f46821ce
XXN <jaechan27.0.2.28@gmail.com>
2025-12-02 22:53:24 +0900

    feat: Success Code 추가

--- payment-service/src/main/java/com/javaauction/payment_service/presentation/advice/PaymentSuccessCode.java
+++ payment-service/src/main/java/com/javaauction/payment_service/presentation/advice/PaymentSuccessCode.java
@@ -13,6 +13,7 @@ public enum PaymentSuccessCode implements ResponseCode {
     WALLET_CHARGE_SUCCESS(HttpStatus.OK, "PAYMENT201", "잔액 충전 성공"),
     WALLET_WITHDRAW_SUCCESS(HttpStatus.OK, "PAYMENT202", "잔액 출금 성공"),
     WALLET_DEDUCT_SUCCESS(HttpStatus.OK, "PAYMENT203", "잔액 차감 성공"),
+    WALLET_VALIDATE_SUCCESS(HttpStatus.OK, "PAYMENT204", "잔액 검증 성공"),
     ;
 
     private final HttpStatus status;
