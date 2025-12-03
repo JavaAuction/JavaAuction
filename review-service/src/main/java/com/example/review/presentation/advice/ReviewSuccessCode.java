6f0a528cac01cb01eda76e39ce057808a33c5ce3
hangw2130\hangw <hangw2130@gmail.com>
2025-12-03 10:52:41 +0900

    fix: 응답코드 수정

--- review-service/src/main/java/com/example/review/presentation/advice/ReviewSuccessCode.java
+++ review-service/src/main/java/com/example/review/presentation/advice/ReviewSuccessCode.java
@@ -8,11 +8,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ReviewSuccessCode implements ResponseCode {
-    REVIEW_CREATED(HttpStatus.CREATED, "USER-201", "리뷰 생성이 완료되었습니다."),
-    REVIEW_LIST_FOUND(HttpStatus.OK, "USER-210", "리뷰 목록 조회 성공"),
-    REVIEW_FOUND(HttpStatus.OK, "USER-211", "리뷰 조회 성공"),
-    REVIEW_UPDATED(HttpStatus.OK, "USER-220", "리뷰 정보 수정 성공"),
-    REVIEW_DELETED(HttpStatus.OK, "USER-230", "리뷰 삭제 성공");
+    REVIEW_CREATED(HttpStatus.CREATED, "REVIEW-201", "리뷰 생성이 완료되었습니다."),
+    REVIEW_LIST_FOUND(HttpStatus.OK, "REVIEW-210", "리뷰 목록 조회 성공"),
+    REVIEW_FOUND(HttpStatus.OK, "REVIEW-211", "리뷰 조회 성공"),
+    REVIEW_UPDATED(HttpStatus.OK, "REVIEW-220", "리뷰 정보 수정 성공"),
+    REVIEW_DELETED(HttpStatus.OK, "REVIEW-230", "리뷰 삭제 성공");
 
     private final HttpStatus status;
     private final String code;
