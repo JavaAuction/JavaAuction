67fe2a14629b85f3524c9c365182f5d8e9e6bbff
banasu0723 <banasu0723@gmail.com>
2025-12-03 16:10:02 +0900

    feat: product category 엔티티및 레포지토리 구현

---
+++ product-service/src/main/java/com/springcloud/eureka/client/productservice/domain/entity/ProductCategory.java
@@ -0,0 +1,30 @@
+package com.springcloud.eureka.client.productservice.domain.entity;
+
+import com.javaauction.global.infrastructure.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@Entity
+@Table(name = "p_product_category")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProductCategory extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "category_id", nullable = false, updatable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false, unique = true, length = 50)
+    private String name;
+
+    public static ProductCategory create(String name) {
+        ProductCategory category = new ProductCategory();
+        category.name = name;
+        return category;
+    }
+}
