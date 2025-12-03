66ba2ded3c9352e56c5e2f95b0cf4520333f3dc4
taeaeaeae <haenimhana@gmail.com>
2025-12-03 15:01:11 +0900

    feat : 어노테이션 추가

--- auction-service/src/main/java/com/javaauction/auction_service/application/service/impl/AuctionServiceImpl.java
+++ auction-service/src/main/java/com/javaauction/auction_service/application/service/impl/AuctionServiceImpl.java
@@ -145,6 +145,8 @@ public class AuctionServiceImpl implements AuctionService {
         auction.updateStatus(req.status());
     }
 
+    @Transactional
+    @Override
     public ResBuyNowDto buyNow(UUID auctionId, String user) {
         Auction auction = auctionRepository.findByAuctionIdAndDeletedAtIsNull(auctionId)
             .orElseThrow(() -> new BussinessException(AuctionErrorCode.AUCTION_NOT_FOUND));
@@ -167,15 +169,15 @@ public class AuctionServiceImpl implements AuctionService {
         // TODO: 경매 종료, 낙찰 기능 추후 연결
         // 로그 - 추후 삭제 예정
         log.info("[BuyNow] closeAuction(auctionId={}, winnerId={}, finalPrice={})",
-                auctionId, user, price);
+            auctionId, user, price);
 
         return ResBuyNowDto.builder()
-                .auctionId(auctionId)
-                .productId(auction.getProductId())
-                .buyerId(user)
-                .finalPrice(price)
-                .purchasedAt(Instant.now())
-                .build();
+            .auctionId(auctionId)
+            .productId(auction.getProductId())
+            .buyerId(user)
+            .finalPrice(price)
+            .purchasedAt(Instant.now())
+            .build();
     }
 
     @Transactional
