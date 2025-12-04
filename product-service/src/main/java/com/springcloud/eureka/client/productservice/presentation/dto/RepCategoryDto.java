12458d24c0f3580ee8870dff6bbb40072d54312e
banasu0723 <banasu0723@gmail.com>
2025-12-04 12:34:29 +0900

    feat: product category 조회 구현

--- product-service/src/main/java/com/springcloud/eureka/client/productservice/presentation/dto/RepCategoryDto.java
+++ product-service/src/main/java/com/springcloud/eureka/client/productservice/presentation/dto/RepCategoryDto.java
@@ -4,17 +4,23 @@ import com.springcloud.eureka.client.productservice.domain.entity.ProductCategor
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.Instant;
+
 @Getter
 @Builder
 public class RepCategoryDto {
 
     private String categoryId;
     private String name;
+    private Instant createdAt;
+    private Instant updatedAt;
 
     public static RepCategoryDto from(ProductCategory category) {
         return RepCategoryDto.builder()
                 .categoryId(category.getId().toString())
                 .name(category.getName())
+                .createdAt(category.getCreatedAt())
+                .updatedAt(category.getUpdatedAt())
                 .build();
     }
 }
