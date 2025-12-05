3847917c457c661c57d8f3dbc7213545296579d4
XXN <jaechan27.0.2.28@gmail.com>
2025-12-05 14:32:48 +0900

    feat: JPA Auditing 수정

---
+++ payment-service/src/main/java/com/javaauction/payment_service/presentation/constant/HttpHeaderNames.java
@@ -0,0 +1,7 @@
+package com.javaauction.payment_service.presentation.constant;
+
+public class HttpHeaderNames {
+
+    public static final String USERNAME = "X-User-Username";
+    public static final String ROLE = "X-User-Role";
+}
