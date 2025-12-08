a1dd510d07d26a7c4f13c9cc14bcd4df30537524
fleurnari <fleur_nr@naver.com>
2025-12-08 12:32:55 +0900

    fix : chat error code 수정

--- chat-service/src/main/java/com/javaauction/chatservice/presentation/advice/ChatErrorCode.java
+++ chat-service/src/main/java/com/javaauction/chatservice/presentation/advice/ChatErrorCode.java
@@ -7,10 +7,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ChatErrorCode implements ResponseCode {
 
-    CHAT_CHATROOM_NOT_FOUND("ALERT000", "해당 채팅방을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    CHAT_CHATTING_NOT_FOUND("ALERT001", "해당 채팅을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    CHAT_PRODUCT_NOT_FOUND("ALERT002", "해당 상품을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    CHAT_UNAUTH("ALERT003", "자신이 소속된 채팅방의 채팅만 접근 가능합니다.", HttpStatus.UNAUTHORIZED),
+    CHAT_CHATROOM_NOT_FOUND("CHAT000", "해당 채팅방을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    CHAT_CHATTING_NOT_FOUND("CHAT001", "해당 채팅을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    CHATROOM_ACCESS_DENIED("CHAT002", "채팅방 접근 권한이 없습니다.", HttpStatus.FORBIDDEN),
+    CHATTING_ACCESS_DENIED("CHAT003", "채팅 접근 권한이 없습니다.", HttpStatus.FORBIDDEN),
     CHAT_CANNOT_CREATE_WITH_SELF("CHAT004", "자신에게 채팅을 요청할 수 없습니다.", HttpStatus.BAD_REQUEST),
     CHAT_CANNOT_CREATE_DIFF_RECEIVER("CHAT005", "채팅방 소속이 아닌 유저에게 메시지를 전송할 수 없습니다.", HttpStatus.BAD_REQUEST),
     CHAT_PRODUCT_FORBIDDEN("CHAT006", "채팅 요청을 보내는 상대가 상품 등록자와 일치하지 않습니다.", HttpStatus.FORBIDDEN),
