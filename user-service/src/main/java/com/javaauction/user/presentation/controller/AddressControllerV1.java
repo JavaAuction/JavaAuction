9dbdee87a4803ceaccc05f477d74fe2263603501
hangw2130\hangw <hangw2130@gmail.com>
2025-12-17 12:13:02 +0900

    feat: swagger 컨트롤러 설명 추가

--- user-service/src/main/java/com/javaauction/user/presentation/controller/AddressControllerV1.java
+++ user-service/src/main/java/com/javaauction/user/presentation/controller/AddressControllerV1.java
@@ -8,6 +8,8 @@ import com.javaauction.user.domain.entity.AddressEntity;
 import com.javaauction.user.infrastructure.JWT.JwtUserContext;
 import com.javaauction.user.presentation.advice.UserSuccessCode;
 import com.javaauction.user.presentation.dto.ResGetAddressDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -20,10 +22,12 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/v1/address")
 @RequiredArgsConstructor
+@Tag(name = "사용자 주소 API", description = "사용자 주소 관련 기능 API입니다.")
 public class AddressControllerV1 {
 
     private final AddressServiceV1 addressService;
 
+    @Operation(summary = "주소 생성", description = "입력 정보로 주소를 생성")
     @PostMapping("/users/{userId}")
     public ResponseEntity<ApiResponse<Void>> createAddress(
             @PathVariable String userId,
@@ -34,6 +38,7 @@ public class AddressControllerV1 {
                 .body(ApiResponse.success(UserSuccessCode.ADDRESS_CREATED));
     }
 
+    @Operation(summary = "주소 목록 조회", description = "주소 목록을 조회(관리자만 가능)")
     @GetMapping()
     public ResponseEntity<ApiResponse<Object>> getAllAddress(
             @RequestParam(value = "page", defaultValue = "1") int page,
@@ -44,12 +49,14 @@ public class AddressControllerV1 {
         return ResponseEntity.ok(ApiResponse.success(UserSuccessCode.ADDRESS_LIST_FOUND, addressService.getAddress(page - 1, size, sortBy, isAsc, JwtUserContext.getRoleFromHeader(), JwtUserContext.getUsernameFromHeader())));
     }
 
+    @Operation(summary = "주소 수정", description = "입력 정보로 주소를 수정(수정할 값만 입력)")
     @PostMapping("/{addressId}")
     public ResponseEntity<ApiResponse<Void>> updateAddress(@PathVariable UUID addressId, @RequestBody ReqUpdateAddressDto req) {
         addressService.updateAddress(addressId, req, JwtUserContext.getUsernameFromHeader());
         return ResponseEntity.ok(ApiResponse.success(UserSuccessCode.ADDRESS_UPDATED));
     }
 
+    @Operation(summary = "주소 삭제", description = "해당 주소를 삭제")
     @DeleteMapping("/{addressId}")
     public ResponseEntity<ApiResponse<Void>> deleteAddress(@PathVariable UUID addressId) {
         addressService.deleteAddress(addressId, JwtUserContext.getUsernameFromHeader());
