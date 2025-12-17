081103b9eca71a0ffdfb530b04bd29cca7052f65
cicle00 <cicle1999@gmail.com>
2025-12-17 11:11:23 +0900

    del: 테스트 api 삭제

--- auction-service/src/main/java/com/javaauction/auction_service/presentation/controller/BidController.java
+++ auction-service/src/main/java/com/javaauction/auction_service/presentation/controller/BidController.java
@@ -1,7 +1,6 @@
 package com.javaauction.auction_service.presentation.controller;
 
 import com.javaauction.auction_service.application.service.BidService;
-import com.javaauction.auction_service.domain.entity.Bid;
 import com.javaauction.auction_service.infrastructure.repository.BidRepository;
 import com.javaauction.auction_service.presentation.advice.BidSuccessCode;
 import com.javaauction.auction_service.presentation.dto.request.ReqPostBidDto;
@@ -12,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -68,12 +66,5 @@ public class BidController {
         return ResponseEntity.ok(ApiResponse.success(BidSuccessCode.BID_FIND_SUCCESS,
                 bidService.getBids(auctionId)));
     }
-
-    // 상태 테스트용 조회 api - 추후 삭제 예정
-    @GetMapping("/debug/bids")
-    public List<Bid> getAllBids() {
-        return bidRepository.findAll();
-    }
-
 }
 
